a3779a1bbe43e8fe1a39254579fd35596b611480
Will Ahlborn <will.ahlborn@bluesentryit.com>
2025-03-28 22:50:57 -0400

    fixed tag

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -4,7 +4,7 @@ provider "aws" {
   default_tags {
     tags = {
       Project     = "ai_ml_competition_2025_0"
-      Owners      = "Noor"
+      Owners      = "First Order"
       Provisioner = "Terraform"
     }
   }
