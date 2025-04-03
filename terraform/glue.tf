2e3d55cd4155c8d5148f91993989d20acf0849ed
Andrew Huddleston <andrew.huddleston@bscanalytics.com>
2025-04-03 07:59:44 -0500

    glue & sagemaker

---
+++ terraform/glue.tf
@@ -0,0 +1,48 @@
+resource "aws_glue_catalog_database" "glue_db" {
+  name = "first-order-log-analysis-db"
+}
+
+resource "aws_glue_crawler" "glue_crawler" {
+  name          = "first-order-log-crawler"
+  role          = aws_iam_role.glue_role.arn
+  database_name = aws_glue_catalog_database.glue_db.name
+  schedule      = "cron(0 12 * * ? *)"
+
+  s3_target {
+    path = "s3://first-order-application-logs/fluent-bit-logs/"
+  }
+
+  schema_change_policy {
+    delete_behavior = "DELETE_FROM_DATABASE"
+    update_behavior = "UPDATE_IN_DATABASE"
+  }
+}
+
+resource "aws_iam_role" "glue_role" {
+  name = "first-order-aws-glue-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "glue.amazonaws.com"
+        },
+        Action = "sts:AssumeRole",
+      },
+    ],
+  })
+}
+
+resource "aws_iam_role_policy" "glue_policy" {
+  role   = aws_iam_role.glue_role.id
+  policy = data.aws_iam_policy_document.glue_policy_doc.json
+}
+
+data "aws_iam_policy_document" "glue_policy_doc" {
+  statement {
+    actions   = ["s3:GetObject", "s3:PutObject"]
+    resources = ["arn:aws:s3:::first-order-application-logs/*"]
+  }
+}
---
+++ terraform/sagemaker.tf
@@ -0,0 +1,47 @@
+resource "aws_sagemaker_notebook_instance" "sagemaker_notebook" {
+  name          = "first-order-LogAnalysisNotebook"
+  role_arn      = aws_iam_role.sagemaker_role.arn
+  instance_type = "ml.t2.medium"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_iam_role" "sagemaker_role" {
+  name = "first-order-aws-sagemaker-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "sagemaker.amazonaws.com"
+        },
+        Action = "sts:AssumeRole",
+      },
+    ],
+  })
+}
+
+resource "aws_iam_role_policy" "sagemaker_s3_access" {
+  role = aws_iam_role.sagemaker_role.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket",
+        ],
+        Resource = [
+          "arn:aws:s3:::first-order-application-logs/*",
+          "arn:aws:s3:::first-order-application-logs",
+        ],
+      },
+    ],
+  })
+}
